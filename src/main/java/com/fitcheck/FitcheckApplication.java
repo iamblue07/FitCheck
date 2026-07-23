f6aa5964a8db1430fd3f53ecd231e241589af347
Dani <Dani@DESKTOP-0C6QAO7>
2026-07-23 22:43:55 +0300

    Initial project setup

---
+++ src/main/java/com/fitcheck/FitcheckApplication.java
@@ -0,0 +1,13 @@
+package com.fitcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitcheckApplication.class, args);
+	}
+
+}
