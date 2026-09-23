06df5242306830966218d0756de17e362dc31520
Dani <Dani@DESKTOP-0C6QAO7>
2026-09-23 21:09:20 +0300

    Prepping for deployment

--- src/test/java/com/sewlect/SewlectApplicationTests.java
+++ src/test/java/com/sewlect/SewlectApplicationTests.java
@@ -1,13 +1,25 @@
 package com.sewlect;
 
+import com.sewlect.support.AbstractPostgresIntegrationTest;
+import com.sewlect.support.WebSliceTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SewlectApplicationTests {
+@SpringBootTest(properties = {
+		"spring.ai.model.chat=none",
+		"spring.ai.model.embedding=none",
+		WebSliceTestConfig.JWT_SECRET_PROPERTY,
+		"r2.endpoint=https://r2.test.invalid",
+		"r2.access-key-id=test",
+		"r2.secret-access-key=test",
+		"r2.bucket=test",
+		"fashn.api-key=test",
+		"ollama.cloud.api-key=test",
+		"deepinfra.api-key=test"
+})
+class SewlectApplicationTests extends AbstractPostgresIntegrationTest {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
